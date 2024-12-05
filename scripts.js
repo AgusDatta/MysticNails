6654d6589b7448833526ae8859c569e8078ccc6c
Agustin Dattilio <agustind@siainteractive.com>
2024-12-05 02:44:05 -0300

    base estructural

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.querySelectorAll("details").forEach((detail) => {
+    detail.addEventListener("toggle", () => {
+      if (detail.open) {
+        document.querySelectorAll("details").forEach((other) => {
+          if (other !== detail) {
+            other.removeAttribute("open");
+          }
+        });
+      }
+    });
+  });
+  
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          green: {
+            50: '#F2FDF2',
+            100: '#E6F8E6',
+            200: '#C4ECC4',
+            600: '#4CAF50',
+            700: '#388E3C',
+            800: '#2E7D32',
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
